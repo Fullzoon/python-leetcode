16de3c7788188869b2e307e99712d928b0815d84
满月 <fullzoon@qq.com>
2026-04-17 09:46:29 +0800

    feat: 完成169.多数元素

--- 169.多数元素.py
+++ 169.多数元素.py
@@ -3,3 +3,23 @@
 
 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
 '''
+
+
+def majorityElement(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    obj = {}
+    for num in nums:
+        if num not in obj:
+            obj[num] = 1
+        else:
+            obj[num] += 1
+        if obj[num] > len(nums)/2:
+            return num
+
+
+nums1 = [2,2,1,1,1,2,2]
+
+print(majorityElement(nums1))
